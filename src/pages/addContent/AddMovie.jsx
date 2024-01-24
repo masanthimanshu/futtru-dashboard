2bbf24a7df1e9b4f727f70474f5a7aff6111baff
himanshumasant <himanshu.masant@wisepromo.com>
2024-01-24 19:02:14 +0530

    updates

--- src/pages/addContent/AddMovie.jsx
+++ src/pages/addContent/AddMovie.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
-import { TextField, Grid, Typography, Button } from "@mui/material";
+import {
+  Grid,
+  Button,
+  Select,
+  Divider,
+  MenuItem,
+  TextField,
+  Typography,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 
 export default function AddMovie() {
+  const [url, setUrl] = useState("");
+  const [type, setType] = useState("");
+  const [desc, setDesc] = useState("");
   const [title, setTitle] = useState("");
 
   const handleSubmit = (e) => {
@@ -14,17 +27,63 @@ export default function AddMovie() {
         <b>Add Web Series</b>
       </Typography>
       <br />
+      <Divider />
+      <br />
+      <br />
       <form onSubmit={handleSubmit}>
         <Grid container spacing={5}>
           <Grid item md={6}>
+            <img src="/upload-image.png" alt="Upload" />
+          </Grid>
+          <Grid item md={6}>
             <TextField
-              label="Title"
               value={title}
-              variant="standard"
-              placeholder="Enter Web Series Name"
+              label="Title"
+              placeholder="Enter Movie Name"
               onChange={(e) => setTitle(e.target.value)}
               fullWidth
+              required
             />
+            <br />
+            <br />
+            <TextField
+              value={url}
+              label="URL"
+              placeholder="Paste HLS url here"
+              onChange={(e) => setUrl(e.target.value)}
+              fullWidth
+              required
+            />
+            <br />
+            <br />
+            <TextField
+              rows={4}
+              value={desc}
+              label="Description"
+              placeholder="Enter Movie Description"
+              onChange={(e) => setDesc(e.target.value)}
+              multiline
+              fullWidth
+              required
+            />
+            <br />
+            <br />
+            <br />
+            <Divider />
+            <br />
+            <Typography>Is this movie Free or Paid</Typography>
+            <br />
+            <FormControl fullWidth required>
+              <InputLabel>Type</InputLabel>
+              <Select
+                value={type}
+                label="Type"
+                onChange={(e) => setType(e.target.value)}
+              >
+                <MenuItem value="Free">Free</MenuItem>
+                <MenuItem value="Paid">Paid</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
         <br />
