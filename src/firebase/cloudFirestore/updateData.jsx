2f108e676da934ff474921d8d3d34307fe2282b9
himanshumasant <himanshu.masant@wisepromo.com>
2024-01-30 15:01:27 +0530

    updates

--- src/firebase/cloudFirestore/updateData.jsx
+++ src/firebase/cloudFirestore/updateData.jsx
@@ -1,5 +1,5 @@
-import { updateDoc, arrayUnion, doc } from "firebase/firestore";
 import { db } from "../firebase";
+import { updateDoc, arrayUnion, doc, arrayRemove } from "firebase/firestore";
 
 // Update a document to a Collection
 export const updateDocument = async (collectionName, docId, data) => {
@@ -10,11 +10,30 @@ export const updateDocument = async (collectionName, docId, data) => {
   return res;
 };
 
-// Update arrays of a Collection
-export const updateArray = async (collectionName, docId, arrName, value) => {
-  const res = await updateDoc(doc(db, collectionName, docId), {
-    [arrName]: arrayUnion(value),
-  });
+// Add value to an arrays of a Document
+export const updateArrayAdd = async (collectionName, docId, arrName, value) => {
+  try {
+    await updateDoc(doc(db, collectionName, docId), {
+      [arrName]: arrayUnion(value),
+    });
 
-  return res;
+    return "Array Updated";
+  } catch (e) {
+    console.log(e);
+    return "Error Updating Array";
+  }
+};
+
+// Remove value from an array of a Document
+export const updateArrayRemove = async (collection, docId, arrName, value) => {
+  try {
+    await updateDoc(doc(db, collection, docId), {
+      [arrName]: arrayRemove(value),
+    });
+
+    return "Array Updated";
+  } catch (e) {
+    console.log(e);
+    return "Error Updating Array";
+  }
 };
--- src/pages/metadata/Metadata.jsx
+++ src/pages/metadata/Metadata.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { Delete } from "@mui/icons-material";
-import { updateArray } from "../../firebase/cloudFirestore/updateData";
 import { getDocumentData } from "../../firebase/cloudFirestore/getData";
+import {
+  updateArrayAdd,
+  updateArrayRemove,
+} from "../../firebase/cloudFirestore/updateData";
 import {
   Box,
   Grid,
@@ -14,44 +17,62 @@ import {
 
 export default function Metadata() {
   const [tag, setTag] = useState("");
-  const [data, setData] = useState([]);
+  const [genre, setGenre] = useState("");
+  const [allTags, setAllTags] = useState([]);
+  const [allGenres, setAllGenres] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleTagSubmission = (e) => {
     e.preventDefault();
 
     setTag("");
-    setData([...data, tag]);
-    updateArray("metadata", "tags", "tags", tag);
+    setAllTags([...allTags, tag]);
+    updateArrayAdd("metadata", "tags", "tags", tag);
+  };
+
+  const handleGenreSubmission = (e) => {
+    e.preventDefault();
+
+    setGenre("");
+    setAllGenres([...allGenres, genre]);
+    updateArrayAdd("metadata", "genres", "genres", genre);
   };
 
   useEffect(() => {
-    getDocumentData("metadata", "tags").then((res) => setData(res.tags));
+    getDocumentData("metadata", "tags").then((res) => setAllTags(res.tags));
+    getDocumentData("metadata", "genres").then((res) => {
+      setAllGenres(res.genres);
+    });
   }, []);
 
   return (
     <>
       <Typography textAlign="center">Tags Data</Typography>
       <br />
-      <Divider />
-      <br />
       <br />
       <Grid container spacing={2}>
-        {data &&
-          data.map((e, index) => {
+        {allTags &&
+          allTags.map((e, index) => {
             return (
               <Grid item md={2} key={index}>
                 <Box
-                  px={2}
-                  py={1}
+                  p={1}
                   color="white"
                   display="flex"
                   bgcolor="black"
-                  borderRadius={6}
+                  borderRadius={10}
                   alignItems="center"
                   justifyContent="space-around"
                 >
                   <Typography>{e}</Typography>
-                  <IconButton onClick={() => {}} color="error">
+                  <IconButton
+                    color="info"
+                    onClick={() => {
+                      const arr = allTags.filter((item) => item !== e);
+                      setAllTags(arr);
+
+                      updateArrayRemove("metadata", "tags", "tags", e);
+                    }}
+                  >
                     <Delete />
                   </IconButton>
                 </Box>
@@ -61,13 +82,13 @@ export default function Metadata() {
       </Grid>
       <br />
       <br />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleTagSubmission}>
         <Grid container spacing={5}>
           <Grid item md={8}>
             <TextField
               label="Tag"
               value={tag}
-              placeholder="Enter Your Tag"
+              placeholder="Enter Content Tag"
               onChange={(e) => setTag(e.target.value)}
               fullWidth
               required
@@ -89,15 +110,73 @@ export default function Metadata() {
       </form>
       <br />
       <br />
+      <br />
+      <br />
       <Divider />
       <br />
       <br />
       <Typography textAlign="center">Genre Data</Typography>
       <br />
       <br />
-      <Divider />
+      <Grid container spacing={2}>
+        {allGenres &&
+          allGenres.map((e, index) => {
+            return (
+              <Grid item md={2} key={index}>
+                <Box
+                  p={1}
+                  color="white"
+                  display="flex"
+                  bgcolor="black"
+                  borderRadius={10}
+                  alignItems="center"
+                  justifyContent="space-around"
+                >
+                  <Typography>{e}</Typography>
+                  <IconButton
+                    color="info"
+                    onClick={() => {
+                      const arr = allGenres.filter((item) => item !== e);
+                      setAllGenres(arr);
+
+                      updateArrayRemove("metadata", "genres", "genres", e);
+                    }}
+                  >
+                    <Delete />
+                  </IconButton>
+                </Box>
+              </Grid>
+            );
+          })}
+      </Grid>
       <br />
       <br />
+      <form onSubmit={handleGenreSubmission}>
+        <Grid container spacing={5}>
+          <Grid item md={8}>
+            <TextField
+              label="Genre"
+              value={genre}
+              placeholder="Enter Content Genre"
+              onChange={(e) => setGenre(e.target.value)}
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item md={4}>
+            <Box display="flex" alignItems="center" height="100%">
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Add
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
     </>
   );
 }
