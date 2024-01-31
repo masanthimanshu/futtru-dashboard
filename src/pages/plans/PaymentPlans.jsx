e92430bce79172762ea2eb84b9103b4aa600d190
himanshumasant <himanshu.masant@wisepromo.com>
2024-01-31 11:10:55 +0530

    updates

--- src/pages/plans/PaymentPlans.jsx
+++ src/pages/plans/PaymentPlans.jsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+import { Box, Button } from "@mui/material";
+
 export default function PaymentPlans() {
-  return <div>Payment Plans</div>;
+  return (
+    <>
+      <Box display="flex" justifyContent="flex-end">
+        <Link to="#">
+          <Button color="primary" variant="contained">
+            + &nbsp; Add Plans
+          </Button>
+        </Link>
+      </Box>
+    </>
+  );
 }
